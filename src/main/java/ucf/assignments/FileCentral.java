58138a42f018c0411ddd26f0eb6ad8aaa6a33299
Peachey-Desktop <the1peachey@gmail.com>
2021-07-05 23:32:46 -0400

    added more pseudocode

--- src/main/java/ucf/assignments/FileCentral.java
+++ src/main/java/ucf/assignments/FileCentral.java
@@ -1,4 +1,21 @@
 package ucf.assignments;
 
 public class FileCentral {
+
+    public void saveSingleList(){
+
+    }
+
+    public void saveAll(){
+
+    }
+
+    public void loadSingleList(){
+
+    }
+
+    public void loadMultiList(){
+
+    }
+
 }
--- src/main/java/ucf/assignments/ListCentral.java
+++ src/main/java/ucf/assignments/ListCentral.java
@@ -1,4 +1,58 @@
 package ucf.assignments;
 
+import java.util.Map;
+
 public class ListCentral {
+
+    //create a Map of lists that has a Map of items that has a map of item info
+
+    public void newList(){
+        //take in the name of the map
+        //add the name to the class map
+    }
+
+    public void editListTitle(){
+        //take in old title and new title
+        //call newList()
+        //call listCopy()
+    }
+
+    public void listCopy(){
+        //create new map
+        //take data from old map
+        //add data to new map
+        //return new map
+    }
+
+    public void removeList(){
+        //call remove function from Map
+    }
+
+    public void addItem(){
+
+    }
+
+    public void removeItem(){
+
+    }
+
+    public void getItemInfo(){
+
+    }
+
+    public void editItemInfo(){
+
+    }
+
+    public void markComplete(){
+
+    }
+
+    public void markIncomplete(){
+
+    }
+
+    public void sortItems(){
+
+    }
 }
